3afec772fa37006e54687d207293cba2f335feb0
João Vítor <joaovsoutosantos@gmail.com>
2021-08-17 11:39:47 -0300

    [DIA 1] Corrige exercício 04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,21 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const appElement = document.querySelector('[data-js="app"]')
+appElement.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+const appVisibilityToggler = document.querySelector('[data-js="app-visibility-toggler"]')
+
+appVisibilityToggler.addEventListener('click', e => {
+  e.preventDefault()
+
+  const isAppElementHidden = appElement.classList.contains('hide')
+
+  if (isAppElementHidden) {
+    appElement.classList.remove('hide')
+  } else {
+    appElement.classList.add('hide')
+  }
+})
